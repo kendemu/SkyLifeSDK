9d428a936d6b65cc8ab09fbcbd57f4aa1310d3b2
Kensei Demura <kendemu@me.com>
2017-01-31 11:21:06 +0900

    refactoring code #5

---
+++ src/Scratch4D.js
@@ -0,0 +1,43 @@
+import electron from 'electron';
+import DroneHttpServer from "./DroneHttpServer.min";
+
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+
+let mainWindow = null;
+let videoWindow = null;
+
+const PORT = 3001;
+const VPORT = 3002;
+let server = null;
+
+app.on('window-all-closed', () => {
+    if (process.platform != 'darwin'){
+	http.request(
+	    {
+		host: 'localhost',
+		port: '3001',
+		path: '/land'
+	    }
+	    ,(response) => {console.log(response)});
+	app.quit();
+    }
+});
+
+app.on('ready', () => {
+    const DroneServer = new DroneHttpServer(PORT);
+    DroneServer.start();
+    mainWindow = new BrowserWindow({width:1920, height:1080});
+    mainWindow.loadURL('file://' + __dirname + '/../index.html');	
+    mainWindow.on('closed', () => {
+	http.request(
+	    {
+		host: 'localhost',
+		port: '3001',
+		path: '/land'
+	    }
+	    ,(response) => {console.log(response)});
+	
+	mainWindow = null;
+    });    
+});
