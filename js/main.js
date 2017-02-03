f83fb71d60555d81ab174a5f89d172005a186c78
Kensei Demura <kendemu@me.com>
2017-02-03 18:16:09 +0900

    changing project to SkyLifeSDK

--- js/main.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { render } from "react-dom";
-import Scratch4DGrid from "./Scratch4DGrid";
-
-render(
-    <Scratch4DGrid/>,
-    document.getElementById('app')
-);
