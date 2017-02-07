e1b554056bc862a72ea9cdb70518c5e52559b1e5
Kensei Demura <kendemu@me.com>
2017-02-07 12:45:34 +0900

    fixing closing bugs

--- dist/server/Scratch4D.js
+++ dist/server/Scratch4D.js
@@ -25,38 +25,63 @@ var PORT = 3001;
 var VPORT = 3002;
 var server = null;
 
+var force_quit = false;
+
 app.on('window-all-closed', function () {
-				if (process.platform != 'darwin') {
-								_http2.default.request({
-												host: 'localhost',
-												port: '3001',
-												path: '/api/robots/Scratch4D/devices/drone/commands/land'
-								}, function (response) {
-												console.log(response);
-								});
-								app.quit();
-				}
+			if (process.platform != 'darwin') {
+						_http2.default.request({
+									host: 'localhost',
+									port: '3001',
+									path: '/api/robots/Scratch4D/devices/drone/commands/land'
+						}, function (response) {
+									console.log(response);
+						});
+						app.quit();
+			}
+});
+
+app.on('will-quit', function () {
+			console.log("will quit");
+			mainWindow = null;
 });
 
 app.on('ready', function () {
-				mainWindow = new BrowserWindow({ width: 1920, height: 1080, icon: __dirname + '/../drone.ico' });
-				mainWindow.loadURL('file://' + __dirname + '/../view/setup.html');
-				mainWindow.on('closed', function () {
-								console.log("window closed") + _http2.default.request({
-												host: 'localhost',
-												port: '3001',
-												path: '/api/robots/Scratch4D/devices/drone/commands/land'
-								}, function (response) {
-												console.log(response);
-								});
+			mainWindow = new BrowserWindow({ width: 1920, height: 1080, icon: __dirname + '/../drone.ico' });
+			mainWindow.loadURL('file://' + __dirname + '/../view/setup.html');
+			mainWindow.on('closed', function () {
+						console.log("window closed");
+						_http2.default.request({
+									host: 'localhost',
+									port: '3001',
+									path: '/api/robots/Scratch4D/devices/drone/commands/land'
+						}, function (response) {
+									console.log(response);
+						});
 
-								mainWindow = null;
-								app.quit();
-				});
+						mainWindow = null;
+						app.quit();
+			});
+
+			mainWindow.on('close', function (e) {
+						console.log("close");
+						if (!force_quit) {
+									e.preventDefault();
+									mainWindow.hide();
+						}
+			});
+
+			app.on('before-quit', function () {
+						console.log("app wants to quit");
+						force_quit = true;
+			});
+
+			app.on('activate-with-no-open-windows', function () {
+						mainWindow.show();
+			});
 });
 
 ipc.on('drone', function (event, arg) {
-				console.log("ipc called");
-				var DroneServer = new _DroneHttpServer2.default(arg);
-				DroneServer.start();
+			console.log("ipc called");
+			var DroneServer = new _DroneHttpServer2.default(arg);
+			DroneServer.start();
 });
--- src/Scratch4D.js
+++ src/Scratch4D.js
@@ -14,6 +14,8 @@ const PORT = 3001;
 const VPORT = 3002;
 let server = null;
 
+let force_quit = false;
+
 app.on('window-all-closed', () => {
     if (process.platform != 'darwin'){
 	http.request(
@@ -27,11 +29,16 @@ app.on('window-all-closed', () => {
     }
 });
 
+app.on('will-quit', function(){
+    console.log("will quit");
+    mainWindow = null;
+});
+
 app.on('ready', () => {
     mainWindow = new BrowserWindow({width:1920, height:1080, icon: __dirname + '/../drone.ico'});
     mainWindow.loadURL('file://' + __dirname + '/../view/setup.html');
     mainWindow.on('closed', () => {
-	console.log("window closed")+
+	console.log("window closed");
 	http.request(
 	    {
 		host: 'localhost',
@@ -42,7 +49,25 @@ app.on('ready', () => {
 	
 	mainWindow = null;
 	app.quit();
-    });    
+    });
+
+    mainWindow.on('close', (e) => {
+	console.log("close");
+	if(!force_quit){
+	    e.preventDefault();
+	    mainWindow.hide();
+	}
+    });
+    
+    app.on('before-quit', () =>{
+	console.log("app wants to quit");
+	force_quit = true;
+    });
+
+    app.on('activate-with-no-open-windows',() => {
+	mainWindow.show();
+    });
+    
 });
 
 ipc.on('drone', (event, arg) => {
@@ -50,4 +75,3 @@ ipc.on('drone', (event, arg) => {
     const DroneServer = new DroneHttpServer(arg);
     DroneServer.start();
 });
-
